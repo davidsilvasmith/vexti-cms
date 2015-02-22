103d291eccef33bfaf6a453d5ae522c435ebc587
David Smith <dave@davidsilvasmith.com>
2015-02-21 22:17:37 -0500

    fixed some editor css & serving raw markdown

--- dtw/app.js
+++ dtw/app.js
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/raw-markdown', express.static('/Users/smithd98/apps/bitcoinbulls.net/'));
 
 app.use('/', index);
 app.use('/users', users);
