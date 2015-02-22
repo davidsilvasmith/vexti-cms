ca9dccd516026dd579715dc90fa4f541b7534067
David Smith <dave@davidsilvasmith.com>
2015-02-21 21:19:05 -0500

    in memory editing is working

--- dtw/routes/index.js
+++ dtw/routes/index.js
@@ -20,20 +20,10 @@ router.get('*', function (req, res, next) {
 		next();
 	}
 });
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/preview/', function (req, res, next) {
-	var path = '/var/folders/6r/v8g4r3m56m19cq7ljw8x3v_c0000gp/T/jekyll/index.html';
-	if (fs.existsSync(path)) {
-  		res.sendFile(path);
-  	}
-  	else if (fs.existsSync(path) + index.html) {
-  		console.log('checking root: ' + path+index.html);
-  		res.sendFile(path+index.html);
-  	}
+  res.render('index', { title: 'Death to Wordpress' });
 });
 
 module.exports = router;
