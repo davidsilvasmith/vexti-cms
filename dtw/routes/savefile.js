604d450dfa00f053c1d2ecbd3ffbfb235edf30d4
David Smith <dave@davidsilvasmith.com>
2015-03-06 12:16:23 -0500

    updated routes to handle server pathing

--- dtw/routes/savefile.js
+++ dtw/routes/savefile.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 /* GET users listing. */
 router.post('/', function(req, res, next) {
 	var file = req.body.frontmatter + req.body.content;
-	var path = req.body.markdownPath.replace('/raw-markdown/', '/Users/smithd98/apps/bitcoinbulls.net/');
+	var path = '/Users/smithd98/apps/bitcoinbulls.net/' + req.body.markdownPath;
 	fs.writeFile(path, file, function(err) {
 	    if(err) {
 	        res.send('error!!');
