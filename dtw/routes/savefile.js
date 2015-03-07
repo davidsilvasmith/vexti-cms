7f5aeffc5832af85048ed394b3c997bfac8afeed
David Smith <dave@davidsilvasmith.com>
2015-03-07 06:03:50 -0500

    fixed undefined error

--- dtw/routes/savefile.js
+++ dtw/routes/savefile.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-	var file = req.body.frontmatter + req.body.content;
+	var file = req.body.content;
 	var path = '/Users/smithd98/apps/bitcoinbulls.net/' + req.body.markdownPath;
 	fs.writeFile(path, file, function(err) {
 	    if(err) {
