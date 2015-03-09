37f9989038b05936248f10dcb2aed62a2172788e
David Smith <dave@davidsilvasmith.com>
2015-03-09 11:27:38 -0400

    recursive pathing and ignoring some files

--- dtw/routes/index.js
+++ dtw/routes/index.js
@@ -1,6 +1,39 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var path = require('path');
+
+var walk = function(dir, done) {
+	walkInternal(dir, dir, done);
+};
+
+var walkInternal = function(dir, originalDir, done) {
+  var results = [];
+  fs.readdir(dir, function(err, list) {
+    if (err) return done(err);
+    var pending = list.length;
+    if (!pending) return done(null, results);
+    list.forEach(function(file) {
+      if (file == 'node_modules' || file.indexOf(".") == 0) {
+        console.log('not including:', file);
+        if (!--pending) done(null, results);
+        return;
+      }
+      file = path.resolve(dir, file);
+      fs.stat(file, function(err, stat) {
+        if (stat && stat.isDirectory()) {
+          walkInternal(file, originalDir, function(err, res) {
+            results = results.concat(res);
+            if (!--pending) done(null, results);
+          });
+        } else {
+          results.push(file.replace(originalDir, ''));
+          if (!--pending) done(null, results);
+        }
+      });
+    });
+  });
+};
 
 router.get('*', function (req, res, next) {
 	console.log('hostname:' + req.hostname);
@@ -11,12 +44,21 @@ router.get('*', function (req, res, next) {
 		var websitePath = '/var/folders/6r/v8g4r3m56m19cq7ljw8x3v_c0000gp/T/jekyll/';
 		var path= websitePath + req.url;
 
-		var filePath = '/Users/smithd98/apps/bitcoinbulls.net/_posts/';
+		var filePath = '/Users/smithd98/apps/bitcoinbulls.net/';
 		console.log('path:' + path);
 
 		if (req.url.indexOf('/system/dir') == 0){
-			var files = fs.readdirSync(filePath);
-			res.send(files);
+			var finish = function (error, results) {
+				if(error == null){
+					res.send(results);
+				}
+				else {
+					res.send(error);
+				}
+			}
+			walk(filePath, finish);
+			//var files = fs.readdirSync(filePath);
+			//res.send(files);
 			return;
 		}
 		if (req.url.indexOf('/system/file') == 0) {
